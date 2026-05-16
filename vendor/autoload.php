f0153069b406b52f44dce6ba1d5f32a7ab17d5be
Bilal Khawaja <imbilalkhawaja@gmail.com>
2026-05-16 15:49:11 +0500

    Use production vendor for shared hosting

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit36793882908b6cbdd344b5a4840231db::getLoader();
+return ComposerAutoloaderInit7950e452adc84bae26397b56acdf0b1e::getLoader();
